d2cb410dbeb7fd79965b27c0888e8d4e46c0143e
Andres <alcivar.andres001@hotmail.com>
2025-12-28 22:57:59 -0500

    se parametriza subida de imagenes

--- src/utils/cloud_storage.ts
+++ src/utils/cloud_storage.ts
@@ -1,14 +1,23 @@
 import { Storage } from '@google-cloud/storage';
 import { format } from 'util';
 import { v4 as uuidv4 } from 'uuid';
+import 'dotenv/config';
 
 const uuid = uuidv4();
 
 const storage = new Storage({
   projectId: 'ecommerce-imagenes-3b10a',
-  keyFilename: './serviceAccountKey.json',
+  //keyFilename: './serviceAccountKey.json',
+  credentials: {
+    client_email: process.env.GCP_CLIENT_EMAIL,
+    private_key: process.env.GCP_PRIVATE_KEY?.replace(/\\n/g, '\n'),
+  },
 });
 
+console.log('ENV CHECK:', {
+  email: !!process.env.GCP_CLIENT_EMAIL,
+  key: !!process.env.GCP_PRIVATE_KEY,
+});
 const bucket = storage.bucket('ecommerce-imagenes-3b10a.firebasestorage.app');
 
 /**
