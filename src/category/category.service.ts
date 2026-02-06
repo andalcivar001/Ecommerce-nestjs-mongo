a3ae80f5c946deec27d8c8dd7c8e58b578d9012b
aalcivar <aalcivar@plapasa.com>
2026-02-06 12:31:15 -0500

    se ordena el findall por descripcion

--- src/category/category.service.ts
+++ src/category/category.service.ts
@@ -23,7 +23,7 @@ export class CategoryService {
   }
 
   async findAll() {
-    return await this.categoryModel.find();
+    return await this.categoryModel.find().sort({ descripcion: 1 });
   }
 
   async findById(id: string) {
